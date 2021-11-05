9088a4d40ef8a491eec797e5abd084a70f326bea
makaulucky <makaulucky5@gmail.com>
2021-11-05 08:57:36 +0300

    Initial commit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Text, View, StyleSheet} from 'react-native'
-import RectButton from "react-native-gesture-handler"
-
+import {RectButton} from "react-native-gesture-handler"
 
 interface ButtonProps {
     variant: "default" | "primary";
